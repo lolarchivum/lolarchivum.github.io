9d09936ee75566712b51285cdbbd7eb3051fb720
Nemin32 <pergerlori@gmail.com>
2020-02-20 22:50:31 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,43 @@
+const pagesize = 25;
+
+function add_elem(root, curr) {
+    let elem = document.createElement("div")
+    elem.className = "comment"
+    elem.innerHTML = `<h3>${curr.poster} (+${curr.up_votes}/-${curr.down_votes}) | ${curr.date}</h3><p>${curr.body.replace(/\n/g,"</p><p>")}</p>`
+
+    root.appendChild(elem)
+
+    if (curr.replies.length > 0) {
+	elem.innerHTML += "<div class='replies'></div>"
+	let replies = elem.querySelector(".replies")
+
+	curr.replies.forEach(r => {
+	    add_elem(replies, r)
+	})
+    }
+}
+
+for (let i = 0; i < pagesize; i++) {
+    fetch(`./Top1000/${i}.json`).then(resp => resp.json()).then(json => {
+	document.getElementById("root").innerHTML += `<h1><a href="./Top100/${i}.json">${json.title}</a></h1>`
+	add_elem(document.getElementById("root"), json)
+    })
+}
+
+let count = pagesize
+
+document.querySelector("#btn").addEventListener("click", () => {
+    document.getElementById("root").innerHTML = ""
+    for (let i = count; i < count+pagesize; i++) {
+	fetch(`./Top1000/${i}.json`).then(resp => resp.json()).then(json => {
+	    document.getElementById("root").innerHTML += `<h1><a href="./Top100/${i}.json">${json.title}</a></h1>`
+	    add_elem(document.getElementById("root"), json)
+	})
+    }
+
+    count += pagesize
+})
+
+document.querySelector("#top").addEventListener("click", () => {
+    window.scrollTo(0,0);
+})
